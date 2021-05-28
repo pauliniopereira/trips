f98fc55c6a37937e5549563bfa5c66c20a37330a
pauliniopereira <84206561+pauliniopereira@users.noreply.github.com>
2021-05-28 19:55:39 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const smoothLinks = document.querySelectorAll('a[href^="#"]');
-console.log(smoothLinks);
 for (let smoothLink of smoothLinks) {
   smoothLink.addEventListener("click", function (e) {
     e.preventDefault();
     const id = smoothLink.getAttribute("href");
-    console.log(id);
 
     document.querySelector(id).scrollIntoView({
       block: "start",
-      behavior: "smooth"
     });
   });
 }
